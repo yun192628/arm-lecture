233064b67a6e9df44fce9a95e402f19746d0e31a
yun <yun@crunchbang.(none)>
2014-08-04 14:58:45 +0800

    Complete C code

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -11,4 +11,14 @@ int main(int argc, char **argv)
   result = fibonacci(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
+#if 0
+int fibonacci(int x){
+	if(x <= 0 )
+		return 0;
+	if(x == 1)
+		return 1;
+	else
+		return fibonacci(x-1) + fibonacci(x-2);
+}
+#endif
 
